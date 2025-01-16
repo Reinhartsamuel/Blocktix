dca0b9022e73952372e9084f8aa8212867737766
Reinhart Samuel <smuel.rr@gmail.com>
2025-01-17 04:35:34 +0700

    change heading

--- app/page.tsx
+++ app/page.tsx
@@ -44,7 +44,7 @@ export default function Home() {
           <div className="flex flex-col items-center space-y-4 text-center">
             <div className="space-y-2">
               <h1 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl lg:text-6xl/none">
-                The Future of Event Ticketing
+               lu.ma, but web3
               </h1>
               <p className="mx-auto max-w-[700px] text-gray-400 md:text-xl">
                 Secure, transparent, and verifiable event tickets powered by blockchain technology.
